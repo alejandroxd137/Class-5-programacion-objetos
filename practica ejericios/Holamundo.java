ef267e63451b41ad29b885301905369a6378616f
alejandro <259216818+alejandroxd137@users.noreply.github.com>
2026-03-04 20:08:43 +0000

    mundo

---
+++ practica ejericios/Holamundo.java
@@ -0,0 +1,25 @@
+
+
+public class Holamundo {
+    
+    public static void main(String[] args) {
+        //Operaciones de asignacion
+
+        int edad = 32;
+        double estatura = 1.67;
+        boolean tieneCarnet = true;
+        char letra = 'L';
+        String direccion = "Suscribite 123";
+        long numeroLargo =98654345;
+
+        System.out.println ( "Hola! Mi edad es: " + edad );
+        System.out.println ( "Mi estatura es: " + estatura );
+        System.out.println ( "Tiene carnet: " + tieneCarnet );
+       System.out.println ( "Direccion: " + direccion );
+
+
+
+    
+        
+    }
+}
