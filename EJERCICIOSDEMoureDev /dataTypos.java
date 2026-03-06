135eeb91734cde9a5b4325128c3cd4895696cebf
alejandro <259216818+alejandroxd137@users.noreply.github.com>
2026-03-06 17:42:21 +0000

    operadores

---
+++ EJERCICIOSDEMoureDev /dataTypos.java
@@ -0,0 +1,25 @@
+public class  dataTypos {
+
+  public static void main(String[] args) {
+       
+     //tipos de datos primitivos
+       int myInt = 22;
+       System.out.println(myInt); 
+
+       Double myDouble = 2.77;
+       
+       System.out.println(myDouble);
+
+        char myChar =  'a' ;
+        System.out.println(myChar);
+        
+        boolean myBoolean = true;
+        myBoolean = false; 
+        System.out.println(myBoolean);
+
+       }
+
+ }
+
+
+
---
+++ EJERCICIOSDEMoureDev /operadore.java
@@ -0,0 +1,39 @@
+public class operadore {
+
+   public static void main(String[] args) {
+    
+              var a= 9;
+              var b= 6;
+              System.out.println(a+b);
+               System.out.println(a*b);
+                System.out.println(a-b);
+                 System.out.println(a/b);
+                 System.out.println(a%b);
+
+                a= b;
+                System.out.println(a);
+
+                a= b * 2;
+                System.out.println(a);
+
+               a += 1; // a + a + 1
+               System.out.println(a);
+
+               a -= 1;
+               System.out.println(a);
+               
+               a *= 2;
+               System.out.println(a);
+
+               a /= 2;
+               System.out.println(a);
+                a %= 1;
+                System.out.println(a);
+
+             //comparaciones de valores
+
+            System.out.println(a == b);
+            System.out.println(a ==6);
+        }
+    
+}
